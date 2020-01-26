133fb9d5d76c1ffcff4035668923caa32061ad6e
AlbertoGodino <60323456+AlbertoGodino@users.noreply.github.com>
2020-01-26 18:30:05 +0100

    Juego Carrera Liebre contra Tortuga. 2º DAM
    Trabajo de clase (2º DAM) realizado en C#

---
+++ Carrera/Carrera/Carrera/ImprimeCarrera.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Carrera
+{
+    class ImprimeCarrera
+    {
+        public int imprimeCarrera(ref char[] ptrCL, ref char[] ptrCT, int size)
+        {
+            int [] movimientotortuga = { 2, 2, 2, 2, 2, 2, 1, 1, 0, 0 };
+            int [] movimientoliebre = { 4, 4, 4, 4, 2, 2, 0, 0, -2, -2 };
+            int i, j;
+            int t = 0;
+            int l = 0;
+
+            while(t < size && l < size)
+            {
+                for(i = 0; i < size; i++)
+                {
+                    if(t == i)
+                    {
+                        Console.Write("T");
+                    }
+                    else
+                    {
+                        Console.Write(ptrCL[i]);
+                    }
+                } // fin del for
+                Console.WriteLine();
+
+                for(j = 0; j < size; j++)
+                {
+                    if (l == j)
+                    {
+                        Console.Write("L");
+                        if (l != 0 && l == t)
+                        {
+                            Console.Write("OUCH!!!");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(ptrCT[j]);
+                    }
+                } // fin del for
+                Console.WriteLine();
+
+                // Creamos la semilla para el ramdom
+                int seed = Environment.TickCount;
+                Random rm = new Random(seed);
+
+                t += movimientotortuga[rm.Next(0, 9)];
+                l += movimientoliebre[rm.Next(0, 9)];
+
+                if (t < 1)
+                    t = 1;
+                if (l < 1)
+                    l = 1;
+                if (t > size)
+                    t = size;
+                if (l > size)
+                    l = size;
+
+                System.Threading.Thread.Sleep(250);
+                Console.Clear();
+            } // fin del while
+
+            if (t == size && l == size)
+                return 0;
+            else if (t == size)
+                return 1;
+            else
+                return 2;
+
+        } // Fin del método imprimeCarrara
+    } // Fin de la clase ImprimeCarrera
+}
---
+++ Carrera/Carrera/Carrera/MainCarrera.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Carrera
+{
+    // Para definir variables constantes. Se meten todas aquí
+    static class Constants
+    {
+        public const int TAM = 70;
+    }
+
+
+
+    class MainCarrera
+    {
+        static void Main(string[] args)
+        {
+            // Creamos la semilla para el ramdom
+            int seed = Environment.TickCount;
+            Random rm = new Random(seed);
+
+            char[] carreraliebre = new char[Constants.TAM];
+            char[] carreratortuga = new char[Constants.TAM];
+
+            int final;
+            int i;
+
+            for (i = 0; i < Constants.TAM; i++)
+            {
+                carreraliebre[i] = '_';
+                carreratortuga[i] = '_';
+            }
+
+            Console.WriteLine("BANG !!!!! ");
+            Console.WriteLine("Y ARRANCAN !!!!!!!\n");
+
+            ImprimeCarrera c = new ImprimeCarrera();
+
+            final = c.imprimeCarrera(ref carreraliebre, ref carreratortuga, Constants.TAM);
+
+            if (final == 1)
+                Console.WriteLine("\nGANA LA TORTUGA\n");
+            else if (final == 2)
+                Console.WriteLine("\nGANA LA LIEBRE\n");
+            else
+                Console.WriteLine("\nES UN EMPATE\n");
+
+        }
+    }
+}
